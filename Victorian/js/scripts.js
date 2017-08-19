861a20581c253561fcf62134013dbc6f9eec52de
Vladimir <antonsimeonvladimir@gmail.com>
2017-08-19 14:41:21 +0300

    update

--- Victorian/js/scripts.js
+++ Victorian/js/scripts.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-	
+	var windowWidth = $(window).width();
+	var bgPosition = -1*(140 / (1920 - 1280)  ) * (windowWidth - 1280);
+	$('.top-page').addClass('top-page-show').css('background-position', '0 ' + bgPosition + 'px');
 });
