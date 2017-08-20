c79d9c9f92fcf37ed10431423d6d5f073f5819f8
Vladimir <antonsimeonvladimir@gmail.com>
2017-08-20 14:56:15 +0300

    update

--- Victorian/js/scripts.js
+++ Victorian/js/scripts.js
@@ -3,4 +3,22 @@ $(document).ready(function(){
 	var bgPosition = -1*(92 / (1920 - 1280)  ) * (windowWidth - 1280);
 	// var bgPosition = -1*(140 / (1920 - 1280)  ) * (windowWidth - 1280);
 	$('.top-page').addClass('top-page-show').css('background-position', '0 ' + bgPosition + 'px');
+
+
+
+
+	var $grid = $('.grid').isotope({ filter: '*' });
+	$('.filter-button-group').on( 'click', 'button', function() {
+		var filterValue = $(this).attr('data-filter');
+		$('.filter-button-group button').removeClass('is-checked');
+		$(this).addClass('is-checked');
+		$grid.isotope({ filter: filterValue });
+	});
+	setTimeout(function() {
+		$grid.isotope({ filter: '*' });
+	}, 300)
+
+
 });
+
+
