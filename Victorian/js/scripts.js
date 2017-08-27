c9783ef28676a9fd1d6b01cf1bea0345dc221670
Vladimir <antonsimeonvladimir@gmail.com>
2017-08-27 04:12:06 +0300

    гз

--- Victorian/js/scripts.js
+++ Victorian/js/scripts.js
@@ -16,9 +16,79 @@ $(document).ready(function(){
 	});
 	setTimeout(function() {
 		$grid.isotope({ filter: '*' });
-	}, 300)
+	}, 300);
 
 
+
+
+	$('.fade').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear'
+	});
+
+	$('.slider-quote').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  cssEase: 'linear'
+	});
+
+
+	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: true,
+	  fade: true,
+	  asNavFor: '.slider-nav'
+
+	});
+	$('.slider-nav').slick({
+	  slidesToShow: 5,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: false,
+	  focusOnSelect: true
+	});
+
+    $('.slider-blocks-wrap').slick({
+	  dots: false,
+	  infinite: false,
+	  speed: 300,
+	  slidesToShow: 3,
+	  slidesToScroll: 3,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
+
+	});
 });
 
 
